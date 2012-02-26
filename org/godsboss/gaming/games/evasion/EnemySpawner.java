69f523c94ad7197c0a85d1beb6fc64ee4634cc74
Timo Reitz <godsboss@gmx.de>
2012-02-26 06:58:09 +0100

    Let enemy spawner move in relation to the player.

--- org/godsboss/gaming/games/evasion/EnemySpawner.java
+++ org/godsboss/gaming/games/evasion/EnemySpawner.java
@@ -1,11 +1,14 @@
 package org.godsboss.gaming.games.evasion;
 
+import org.godsboss.gaming.physics2d.Position;
+
 class EnemySpawner{
 	private final Game game;
 	private final double threshold;
 	private final ObjectFactory factory;
 
 	private double value = 0;
+	private Position position;
 
 	public EnemySpawner(double threshold, Game game, ObjectFactory factory){
 		this.threshold = threshold;
@@ -14,6 +17,7 @@ class EnemySpawner{
 
 	public void tick(double seconds){
 		value += seconds;
+		position = game.getPlayer().getPosition().plus(game.getBounds().getSize().times(0.5)).modulo(game.getBounds());
 		if (value >= threshold){
 			value -= threshold;
-			game.addEnemy(factory.createEnemy(game.getPlayer().getPosition().plus(game.getBounds().getSize().times(0.5)).modulo(game.getBounds())));}}}
+			game.addEnemy(factory.createEnemy(position));}}}
