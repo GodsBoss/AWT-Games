2a94ba72e3c6d880930b1bb4fd5556f0ef00660c
Timo Reitz <godsboss@gmx.de>
2012-02-29 19:22:03 +0100

    Fix freaking Mac newlines (grmbl).

--- org/godsboss/gcomp/physics2d/BoundedPositioning.java
+++ org/godsboss/gcomp/physics2d/BoundedPositioning.java
@@ -1,1 +1,15 @@
-package org.godsboss.gcomp.physics2d;import org.godsboss.gaming.ecs.Component;import org.godsboss.gaming.physics2d.Bounds;public class BoundedPositioning implements Component{	private final Positionable positionable;	private final Bounds bounds;	public BoundedPositioning(Positionable positionable, Bounds bounds){		this.positionable = positionable;		this.bounds = bounds;}	public void tick(double seconds){		positionable.moveTo(positionable.getNext().modulo(bounds));}}
+package org.godsboss.gcomp.physics2d;
+
+import org.godsboss.gaming.ecs.Component;
+import org.godsboss.gaming.physics2d.Bounds;
+
+public class BoundedPositioning implements Component{
+	private final Positionable positionable;
+	private final Bounds bounds;
+
+	public BoundedPositioning(Positionable positionable, Bounds bounds){
+		this.positionable = positionable;
+		this.bounds = bounds;}
+
+	public void tick(double seconds){
+		positionable.moveTo(positionable.getNext().modulo(bounds));}}
